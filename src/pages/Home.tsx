d997565ccd90bf65b4ebd7714b2fe71ffdf3fcb7
RS-101 <rasmusemilstampe.skousen@colorado.edu>
2025-03-06 13:07:17 -0700

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
     return (
       <div>
-        <h2>Welcome to my chat-bot</h2>
+        <h2>Welcome to team picker chat-bot</h2>
         <p>some text here </p>
       </div>
     );
